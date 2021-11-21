4a6e6f7803f041af1883b789445621f97f16f6d3
Alexandru Chirita <34884567+alexandruchirita4192@users.noreply.github.com>
2021-11-21 10:33:31 +0200

    Initial SQL Cleaner Helper commit

---
+++ SqlCleanerHelper/SqlCleanForm.Designer.cs
@@ -0,0 +1,106 @@
+﻿
+namespace SqlCleanerHelper
+{
+    partial class SqlCleanForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.tbSqlFile = new System.Windows.Forms.TextBox();
+            this.tbSqlText = new System.Windows.Forms.TextBox();
+            this.btnCleanFile = new System.Windows.Forms.Button();
+            this.btnCleanText = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // tbSqlFile
+            // 
+            this.tbSqlFile.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.tbSqlFile.Location = new System.Drawing.Point(13, 12);
+            this.tbSqlFile.Name = "tbSqlFile";
+            this.tbSqlFile.ReadOnly = true;
+            this.tbSqlFile.Size = new System.Drawing.Size(545, 20);
+            this.tbSqlFile.TabIndex = 0;
+            this.tbSqlFile.MouseClick += new System.Windows.Forms.MouseEventHandler(this.tbSqlFile_MouseClick);
+            // 
+            // tbSqlText
+            // 
+            this.tbSqlText.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.tbSqlText.Location = new System.Drawing.Point(13, 43);
+            this.tbSqlText.Multiline = true;
+            this.tbSqlText.Name = "tbSqlText";
+            this.tbSqlText.Size = new System.Drawing.Size(626, 242);
+            this.tbSqlText.TabIndex = 1;
+            // 
+            // btnCleanFile
+            // 
+            this.btnCleanFile.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.btnCleanFile.Location = new System.Drawing.Point(564, 10);
+            this.btnCleanFile.Name = "btnCleanFile";
+            this.btnCleanFile.Size = new System.Drawing.Size(75, 23);
+            this.btnCleanFile.TabIndex = 3;
+            this.btnCleanFile.Text = "Clean file";
+            this.btnCleanFile.UseVisualStyleBackColor = true;
+            this.btnCleanFile.Click += new System.EventHandler(this.btnCleanFile_Click);
+            // 
+            // btnCleanText
+            // 
+            this.btnCleanText.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.btnCleanText.Location = new System.Drawing.Point(564, 291);
+            this.btnCleanText.Name = "btnCleanText";
+            this.btnCleanText.Size = new System.Drawing.Size(75, 23);
+            this.btnCleanText.TabIndex = 4;
+            this.btnCleanText.Text = "Clean text";
+            this.btnCleanText.UseVisualStyleBackColor = true;
+            this.btnCleanText.Click += new System.EventHandler(this.btnCleanText_Click);
+            // 
+            // SqlCleanForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(651, 321);
+            this.Controls.Add(this.btnCleanText);
+            this.Controls.Add(this.btnCleanFile);
+            this.Controls.Add(this.tbSqlText);
+            this.Controls.Add(this.tbSqlFile);
+            this.Name = "SqlCleanForm";
+            this.Text = "SQL Cleaner";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox tbSqlFile;
+        private System.Windows.Forms.TextBox tbSqlText;
+        private System.Windows.Forms.Button btnCleanFile;
+        private System.Windows.Forms.Button btnCleanText;
+    }
+}
+
---
+++ SqlCleanerHelper/SqlCleanForm.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace SqlCleanerHelper
+{
+    public partial class SqlCleanForm : Form
+    {
+        public SqlCleanForm()
+        {
+            InitializeComponent();
+        }
+
+        private void btnCleanFile_Click(object sender, EventArgs e)
+        {
+            var dialog = new OpenFileDialog()
+            {
+                Filter = "SQL|*.sql",
+                DefaultExt = "sql"
+            };
+            if (dialog.ShowDialog() == DialogResult.OK)
+            {
+                var fileName = dialog.FileName;
+                if (string.IsNullOrEmpty(fileName))
+                    return;
+                var sql = File.ReadAllText(fileName);
+                sql = CleanSql(sql);
+                File.WriteAllText(fileName, sql);
+
+                tbSqlFile.Text = fileName;
+            }
+        }
+
+        private void tbSqlFile_MouseClick(object sender, MouseEventArgs e)
+        {
+            btnCleanFile_Click(sender, EventArgs.Empty);
+        }
+
+        private void btnCleanText_Click(object sender, EventArgs e)
+        {
+            var newSql = CleanSql(tbSqlText.Text);
+            tbSqlText.Text = newSql;
+        }
+
+        private string CleanSql(string sql)
+        {
+            var keywordsList = new List<string> { 
+                "add", "alter", "all", "and", "any", "as", "asc", "select", "by", "bulk", "commit", "tran", "rollback",
+                "between", "backup", "constraint", "column", "case", "check", "create", "count", "drop", "rowcount",
+                "exec", "exists", "table", "database", "delete", "desc", "distinct", "index", "procedure", "revert",
+                "stored", "unique", "view", "default", "foreign", "primary", "key", "from", "where", "group", "join",
+                "left", "right", "full", "inner", "outer", "having", "in", "insert", "into", "is", "null", "not", "like",
+                "limit", "or", "order", "apply", "rownum", "top", "set", "update", "truncate", "union", "values",
+                "declare", "fillfactor", "raiserror", "reconfigure", "freetext", "return", "goto", "break", "function",
+                "case", "identity", "identity_insert", "close", "if", "while", "clustered", "coalesce", "isnull",
+                "collate", "object_id", "continue", "convert", "cast", "kill", "nonclustered", "cursor", "transaction",
+                "nullif", "open", "pivot", "unpivot", "disk", "openquery", "trigger", "double", "option", "dump", "except",
+                "execute", "with", "print", "proc", "exit", "percent", "over", "waitfor", "varying", "deallocate", "go",
+                "use", "int", "bigint", "char", "varchar", "nvarchar", "varbinary", "sysname", "datetime", "numeric",
+                "begin", "end", "else", "on", "sum", "avg", "when", "then", "dense_rank", "binary", "error_number",
+                "error_severity", "error_state", "error_procedure", "error_line", "error_message", "try", "catch",
+                "substring", "charindex", "reverse", "quotename", "fetch_status", "first", "fetch", "dynamic", "log",
+                "getdate", "datediff", "minute", "second", "hour", "returns", "row_number", "bit"
+            };
+
+
+
+            sql = Regex.Replace(sql, "\\w+", (Match m) =>
+            {
+                return keywordsList.Contains(m.Value.ToLower()) ? m.Value.ToUpper() : m.Value;
+            });
+
+            return sql;
+        }
+
+    }
+}
